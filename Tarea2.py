ec8967dbda0cf4881b81cbbccb5a4bb6a4cf6b9e
ing-diaz30 <ing.diazclaudia@gmail.com>
2021-06-01 19:39:50 -0600

    Create Tarea2.py

---
+++ Tarea2.py
@@ -0,0 +1,9 @@
+# Calcular el area de un Circulo
+
+from math import pi
+
+radio = float(input('Escruba el radio del circulo: '))
+
+area = pi * radio ** 2
+
+print('El area del circulo es {}'.format(area))
